9262cd04a6febefa720606a20e1cf9cedb912eca
Martin Dos Santos <plateadodev@gmail.com>
2016-04-03 03:23:37 -0300

    Public API done

---
+++ Poloniex.php
@@ -0,0 +1,103 @@
+<?php
+
+class Poloniex
+{
+    protected $key = '';
+    protected $secret = '';
+
+    /**
+     * Poloniex constructor.
+     * @param null $key
+     * @param null $secret
+     */
+    public function __construct($key = null, $secret = null)
+    {
+        $this->key = $key;
+        $this->secret = $secret;
+    }
+
+    /**
+     * @return array
+     */
+    public function returnTicker()
+    {
+        return $this->queryPublic('returnTicker');
+    }
+
+    /**
+     * @return array
+     */
+    public function return24hVolume()
+    {
+        return $this->queryPublic('return24hVolume');
+    }
+
+    /**
+     * @param null $currenyPair
+     * @return array
+     */
+    public function returnOrderBook($currencyPair = null)
+    {
+        if ($currencyPair === null) {
+            $currencyPair = 'all';
+        }
+        return $this->queryPublic('returnOrderBook&currencyPair=' . $currencyPair);
+    }
+
+    /**
+     * @param null $currenyPair
+     * @param $start (UNIX TIME)
+     * @param $end (UNIX TIME)
+     * @return array
+     */
+    public function returnTradeHistory($currencyPair = null, $start, $end)
+    {
+        if ($currencyPair === null) {
+            $currencyPair = 'all';
+        }
+        return $this->queryPublic('returnOrderBook&currencyPair=' . $currencyPair . '&start=' . $start . '&end=' . $end);
+    }
+
+    /**
+     * @param null $currencyPair
+     * @param $start (UNIX TIME)
+     * @param $end (UNIX TIME)
+     * @param $period (candlestick period in seconds; valid values are 300, 900, 1800, 7200, 14400, and 86400)
+     * @return array
+     */
+    public function returnChartData($currencyPair = null, $start, $end, $period)
+    {
+        if ($currencyPair === null) {
+            $currencyPair = 'all';
+        }
+        return $this->queryPublic('returnChartData&currencyPair=' . $currencyPair . '&start=' . $start . '&end=' . $end . '&period=' . $period);
+    }
+
+    /**
+     * @return array
+     */
+    public function returnCurrencies() {
+        return $this->queryPublic('returnCurrencies');
+    }
+
+    /**
+     * @param $currency (Example: BTC)
+     * @return array
+     */
+    public function returnLoanOrders($currency) {
+        return $this->queryPublic('returnCurrencies&currency='.$currency);
+    }
+
+//        https://poloniex.com/public?command=returnChartData&currencyPair=BTC_XMR&start=1405699200&end=9999999999&period=14400
+
+    /**
+     * @param $command
+     * @return array
+     */
+    private function queryPublic($command)
+    {
+        $uri = file_get_contents("https://poloniex.com/public?command=" . $command);
+        return json_decode($uri, true);
+    }
+
+}
--- README.md
+++ README.md
@@ -1,7 +1,13 @@
 # Poloniex API for PHP
 Just my PHP class to operate Poloniex API :)
 
-Donations:
+# Requeriments:
+PHP & CURL (Only for trade API).
+
+#Status:
+On progress. If you want me to spend more time on this, donations will be motivating
+
+# Donations:
 BTC: 1CurdNyG5SpR24j1ds2fkRnyKCaZQytBhL
 
 Paypal: plateadodev@gmail.com
